e9184fa9627eb06397302812a4ac4b6dfe45318c
abiam19 <67296278+abiam19@users.noreply.github.com>
2022-02-17 08:17:49 -0600

    Update registrarLugar.php
    soluciones out

--- include/registrarLugar.php
+++ include/registrarLugar.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lugares de exposición</title>
     <link rel="stylesheet" href="../css/estilos.css?v=<?php echo(rand()); ?>">
-    <link rel="stylesheet" href="../css/soluciones.css?v=<?php echo(rand()); ?>">
     <script src = " https://unpkg.com/sweetalert/dist/sweetalert.min.js "></script>
 </head>
 <body>
